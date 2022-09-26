5b73cb66779aed352d4d5e7223d944d05ff4475b
Chepngeno-langat <langatkarenn@gmail.com>
2022-09-26 14:51:56 +0300

    Task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    if not my_list:
-        return
-    new_list = [n % 2 == 0 for n in my_list]
-
-    return new_list
+def delete_at(my_list=[], idx=0):
+    if (0 < idx >= len(my_list)):
+        return my_list
+    my_list.remove(my_list[idx])
+    return my_list
