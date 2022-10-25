277aa898ca87bb8a41fe4cd7c54612dae9e7ab5a
Chepngeno-langat <langatkarenn@gmail.com>
2022-10-25 17:14:45 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename=""):
     """
 
    with open(filename, encoding="UTF-8") as file1:
-        content = file1.read()
-        print(content, end="")
+        read_file = file1.read()
+        print(read_file, end="")
     file1.closed
