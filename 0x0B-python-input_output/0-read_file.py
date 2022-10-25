f2b144895af4e11c4fb8f84a7dfcd3422d87d094
Chepngeno-langat <langatkarenn@gmail.com>
2022-10-25 17:05:54 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,13 +2,13 @@
 """ read_file module """
 
 
-def read_line(filename=""):
+def read_file(filename=""):
     """
     function that read specific file
     Args:
          fileName: name of the file
     """
 
-    with open(filename, encoding="UTF8") as file1:
-        content = file1.read()
+    with open(filename, encoding="UTF8", "r") as file1:
+        content = file1.readlines()
         print(content end="")
